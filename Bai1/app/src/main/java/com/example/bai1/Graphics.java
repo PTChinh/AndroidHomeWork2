c2f95ca1f015857a9934768e87277da99be2ca28
PTChinh <PTChinh@users.noreply.github.com>
2019-11-19 15:10:31 +0700

    Bai 1 completed

---
+++ Bai1/app/src/main/java/com/example/bai1/Graphics.java
@@ -0,0 +1,15 @@
+package com.example.bai1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Graphics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GraphicsView(this));
+    }
+}
